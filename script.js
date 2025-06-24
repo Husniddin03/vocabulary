289657f3e4def14df9e4e1ccb85b2034be7d31dd
husniddin <husniddin@debian.debian>
2025-06-24 05:33:55 +0500

    add count funktion

--- script.js
+++ script.js
@@ -205,6 +205,7 @@ function renderVocabulary() {
   elements.vocabularyGrid.style.display = "grid";
   elements.emptyState.style.display = "none";
 
+  let vocabularyCount = 1;
   elements.vocabularyGrid.innerHTML = filteredVocabulary
     .map(
       (item) => `
@@ -221,6 +222,7 @@ function renderVocabulary() {
                                 <path d="M19.07 4.93a10 10 0 0 1 0 14.14M15.54 8.46a5 5 0 0 1 0 7.07"></path>
                             </svg>
                         </button>
+                        ${vocabularyCount++}
                     </div>
                     <div class="word-tags">
                         <span class="tag tag-pos">${escapeHtml(
